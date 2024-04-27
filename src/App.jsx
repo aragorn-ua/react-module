e56e743663f65ad7f86819ae62d75c0f53b01d79
Volodymyr <101939818+aragorn-ua@users.noreply.github.com>
2024-04-27 22:07:10 +0300

    form-articels

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+// import axios from "axios";
+import './App.css'
+import ArticleList from "./components/ArticleList"
+import { fetchArticlesWithTopic } from "./articles-api";
+import {SearchForm} from "./components/SearchForm"
+
+const App = () => {
+	const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+	const handleSearch = async (topic) => {
+    try {
+      setArticles([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchArticlesWithTopic(topic);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <SearchForm onSearch={handleSearch} />
+      {loading && <p>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
+};
+
+
+
+
+export default App;
